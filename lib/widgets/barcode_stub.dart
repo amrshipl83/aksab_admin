18affdd87f55a4d00900239b84725f8f81fe58c0
Amr Shipl <amrshipl09@gmail.com>
2026-03-25 08:06:41 +0000

    Fix delivery dashboard navigation: linked add products and view offers screens

---
+++ lib/widgets/barcode_stub.dart
@@ -0,0 +1,8 @@
+// ملف وهمي لخداع الـ Compiler في نسخة الويب
+class FlutterBarcodeScanner {
+  static Future<String> scanBarcode(String color, String text, bool show, dynamic mode) async {
+    return "-1";
+  }
+}
+enum ScanMode { BARCODE, QR, DEFAULT }
+
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,7 +10,6 @@ import file_picker
 import file_selector_macos
 import firebase_auth
 import firebase_core
-import mobile_scanner
 import path_provider_foundation
 import printing
 import share_plus
@@ -23,7 +22,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-  MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
