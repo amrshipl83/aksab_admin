1ae8b95af5df34e8ba60ca5a986a700208ccb846
amrshipl83 <amrshipl83@users.noreply.github.com>
2026-04-01 07:43:49 +0000

    deploy: c53479d906e9788fb72ea7c422b0cc5c40e48237

--- main.dart.js
+++ main.dart.js
@@ -41168,7 +41168,7 @@ $S:3}
 A.baa.prototype={
 $1(a){var s=A.h1().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/052f31d115eceda8cbff1b3481fcde4330c4ae12/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/425cfb54d01a9472b3e81d9e76fd63a4a44cfbcb/":s)+a},
 $S:70}
 A.a27.prototype={
 gu(a){var s=this.a
