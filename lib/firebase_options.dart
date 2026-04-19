a0f6c56efecafe3c231dfaef16d2d696c3e02db4
Amr Shipl <amrshipl09@gmail.com>
2026-04-19 17:27:31 +0000

    Fix delivery dashboard navigation: linked add products and view offers screens

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,13 +1,20 @@
-{
-  "type": "service_account",
-  "project_id": "aksab-erp",
-  "private_key_id": "67a7a56967ec9237ae720676eb9010e3b5331a9c",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDiqSc7w+Kdvz7G\n/+mnBUCfZsKJ8cZ9ewfYn8Gc9nfI62vuqYQCk0mgFpQncZkvBfH+o5pDFj5KC7LG\nXL9PEzCqt9mSba4W0dubTF00Ft/MVykKP9guM3eFmYVrXnJQo2h8yqrvFDXpPQ9O\nlZaFYN1POcTSo+lIaledUN47WaITHq1p0lhpOP5R7RznI0WL2Tbv84udw671mpl+\nM73HAQonNnGqOq44IOnuR0Y/ERAALpalYjssMclZCkDKUTpw+7LumY97FX46WvHH\n1KCM4adpjNYwn5nmN/LO/gQsTVvoQgmEfBZPIVhkkmi6IZfgLcqWsFAXOg5Krh8J\n+myQn+lBAgMBAAECggEAKpGzVSWuh4r2CuDgbKZvYyClE8xNR57WM+iZL9YYXCte\n64V5zhQEhWUIMTpEF1FKCFWIsOXaEAUPb/LWWU8U2lNKMRaZVh7W3F1kudY79QA/\nrHWeL9sprPVO2gH0fVGbW6iinuFQWLxHhi5RBT7SgcErRpZTR271go3AFyx35JgJ\nPpaR/jNUif7ltb7QlxboFzTzWYhm/U9yBjiFINr+1o0Gxh5ZEua3mH5rNIm10RR5\nYkjevxFfJvGtVP5gxBpzYDL5O2nWYNoakq3fl7uMTGw3jESTOEDvl0YaqUbT8FQ+\nr8HOFl1COtFL53qUuF8+QCgtqIISFwd4+eZfnv7duwKBgQD/NRUxdf6H+V3p1yGt\ne1yP5BSFQlPBQH+g8vOFpNJ2x+x/7qsS5pD15S8oCWeStW3xsI63vA3Nv3lIxNWA\nGic4/vOCEnmqQ94EhCfezlQYBljr53I1kcmDYjDUtRwrKpfCSxvjosmLFYKhARCe\nashUqYCh59gmHeOn6R6wZMxf+wKBgQDjXV9z3kRbgkhi4CiQTFBzoTmvVgY6uxNv\nT6QbnFxmxMH/ZPPyGyr3LqADHCItML+ISlr2weMbd5sM6Pwcyjdfz1dSETjHYC/h\nPKK3P82b3gdgIZhOu+a/iXfm5tGTGCf8HsZFPi0BwEH2cPqB/gClqVUJ4eRykSdE\nzF30cdMK8wKBgGYOdEszRHZb4pKA1ZbqnjlsnEguHhBKGr/iaEApiYC2snz2lLMa\nQR4UaKc6G5EvvKfKU0hmi56b04SmMjroOocwbXC2srDEMyTbNJaFLFvvLeAoziNe\nCZfgzGZDhuA8SDQKlTyS/MYB9BsusdaoHTS+FBKrBGU5zrB3KS6hIJnBAoGAUy6x\nImoaJkSXCR2rmYdiE7vTp/NYGhqhdKL1jmh5YdvwgejEWzpXUNHNFvYvBsuD93AE\nsayj00PCBUokGsBc2/+2mb7VQBRP0B7oBRUhuznTBvejioKyXDcGGAMwf5lh35q+\nvUyPGMnnGCujavWr9O/rvc7YTpYOgNwoL6uJI8MCgYEAzGGKUF+gDmPehwG3gyF9\nsptJpjzUol7eoydG5bOSlSm6EW19S6Wvg2rucb8bE7Iyb7MkfunXDBbUUNw6VHr/\nqlmH/L/GtF6yvAiQtFU7rDaS4PgT4FsV8CTvyjeg6w6XwoVH/ycbQoL8WUaFRzDV\n+CASn1J28pyizJfV31u6DVY=\n-----END PRIVATE KEY-----\n",
-  "client_email": "firebase-adminsdk-fbsvc@aksab-erp.iam.gserviceaccount.com",
-  "client_id": "109464924564571977914",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40aksab-erp.iam.gserviceaccount.com",
-  "universe_domain": "googleapis.com"
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+import 'package:flutter/foundation.dart' show defaultTargetPlatform, kIsWeb, TargetPlatform;
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    return web;
+  }
+
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: 'AIzaSyAR_ilDZbo9hPR0JJ2Wtal555N8VVWvqEQ',
+    authDomain: 'aksab-erp.firebaseapp.com',
+    databaseURL: 'https://aksab-erp-default-rtdb.firebaseio.com',
+    projectId: 'aksab-erp',
+    storageBucket: 'aksab-erp.firebasestorage.app',
+    messagingSenderId: '549455573441',
+    appId: '1:549455573441:web:a198bb1adb5f3b35c4ff40',
+    measurementId: 'G-7GGD70EHNC',
+  );
 }
+
